3f5d298f07981ec16a5a44a1676efaa12c8009dc
Colin Reeder <vpzomtrrfrt@gmail.com>
2017-09-09 16:49:35 -0600

    Size choices

--- src/controller/SketchCanvas.jsx
+++ src/controller/SketchCanvas.jsx
@@ -9,6 +9,9 @@ export default class SketchCanvas extends preact.Component {
 			<div class="colorChoice">
 				{["black", "red", "lime", "blue"].map(color => <ColorChoice color={color} selected={color == this.state.color} change={linkState(this, 'color')} />)}
 			</div>
+			<div class="sizeChoice">
+				{[16, 32, 64, 128].map(size => <SizeChoice size={size} selected={size == this.state.size} change={linkState(this, 'size')} />)}
+			</div>
 		</div>;
 	}
 	offset(canvas, coords) {
@@ -23,6 +26,7 @@ export default class SketchCanvas extends preact.Component {
 	constructor() {
 		super();
 		this.state.color = "black";
+		this.state.size = 16;
 	}
 	componentDidMount() {
 		const canvas = this.base.getElementsByTagName('canvas')[0];
@@ -36,7 +40,7 @@ export default class SketchCanvas extends preact.Component {
 			ctx.moveTo(prev2.x, prev2.y);
 			ctx.lineTo(prev.x, prev.y);
 			ctx.lineTo(now.x, now.y);
-			ctx.lineWidth = 16;
+			ctx.lineWidth = this.state.size;
 			ctx.strokeStyle = this.state.color;
 			ctx.stroke();
 			console.log(now);
@@ -70,5 +74,13 @@ export default class SketchCanvas extends preact.Component {
 class ColorChoice extends preact.Component {
 	render(props, state) {
 		return <div><div class={props.selected ? 'selected' : ''} style={{backgroundColor: props.color}} onClick={props.change.bind(this, props.color)}></div></div>;
+	}
+}
+
+class SizeChoice extends preact.Component {
+	render(props, state) {
+		const oss = (props.size*90/1200)+'vw';
+		const offset = 'calc(50% - '+(props.size*90/2400)+'vw)';
+		return <div><div class={props.selected ? 'selected' : ''} style={{width: oss, height: oss, left: offset, top: offset}} onClick={props.change.bind(this, props.size)}></div></div>;
 	}
 }
